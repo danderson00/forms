ca77cf82d08bb39455b7d5d26c7fb8f35551cce6
Dale Anderson <danderson00@gmail.com>
2019-08-13 00:49:53 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Item from './Item'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [values, setValues] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Item onSubmit={setValues} />
+      <code>
+        {JSON.stringify(values, null, 2)}
+      </code>
     </div>
-  );
+  )
 }
-
-export default App;
